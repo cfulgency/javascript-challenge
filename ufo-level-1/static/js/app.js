e9c5bf3f0e6dfc15a33215068771748de56a1f69
cfulgency <46569502+cfulgency@users.noreply.github.com>
2021-01-09 13:24:34 -0600

    js commit 1

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,4 +1,49 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// Display Dataset as Default
+
+tableData.forEach((report) => {
+    console.log(report);
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+// Select Submit Button
+var submit = d3.select("#filter-btn");
+
+//Click Event of Date/Time Filter
+submit.on("click", function() {
+
+// Remove Existing Table
+d3.select("tbody").html("");
+
+// Prevent Page from Refreshing
+d3.event.preventDefault();
+
+// Get Value Property of Input Element
+var dateTime = d3.select("#datetime").property("value");
+console.log(dateTime);
+
+// Filter Report
+var filteredData = tableData.filter(record => record.datetime === dateTime);
+console.log(filteredData);
+
+// Display Filtered Dataset
+filteredData.forEach((report) => {
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+});
